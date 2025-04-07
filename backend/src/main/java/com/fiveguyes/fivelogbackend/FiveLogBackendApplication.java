8e9669f6b5cde0e665df551f904d826f3356758b
0320kangk <0320kangk@naver.com>
2025-04-07 16:44:43 +0900

    init

---
+++ backend/src/main/java/com/fiveguyes/fivelogbackend/FiveLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fiveguyes.fivelogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveLogBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fiveguyes/fivelogbackend/FiveLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiveguyes.fivelogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveLogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
