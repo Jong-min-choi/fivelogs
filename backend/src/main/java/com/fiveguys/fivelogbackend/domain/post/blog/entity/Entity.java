8421327532ad40db189388219d7f7e0529e52174
thatisbummer <tjdals4358@naver.com>
2025-04-09 18:44:04 +0900

    add: blog entity

---
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/post/blog/entity/Entity.java
@@ -0,0 +1,33 @@
+package com.fiveguys.fivelogbackend.domain.post.blog.entity;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@ToString
+public class Entity {
+    @Id
+    private Long id;
+
+    private String title;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    private LocalDateTime modifiedDate;
+
+}
