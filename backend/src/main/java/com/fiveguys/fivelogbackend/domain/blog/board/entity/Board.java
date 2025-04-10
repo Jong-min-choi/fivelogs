98cb0ea3b242db21591f72d60566cce8f16d117c
Jong-min-choi <juktoma94@naver.com>
2025-04-10 10:38:37 +0900

    create board1

--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/board/entity/Board.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/board/entity/Board.java
@@ -1,4 +1,16 @@
 package com.fiveguys.fivelogbackend.domain.blog.board.entity;
 
+@SuperBuilder
+@MappedSuperClass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@ToString
 public class Board {
+    private String title;
+    private String content;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    private LocalDateTime updatedDate;
 }
