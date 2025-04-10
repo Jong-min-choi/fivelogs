15892c8da3fed19d8c825ecb5b38219812b632ad
dlghwns200 <dlghwns200@gmail.com>
2025-04-10 21:23:47 +0900

    feat 댓글 service controller 수정 #32

--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/comment/dto/CommentRequestDto.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/comment/dto/CommentRequestDto.java
@@ -7,11 +7,17 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class CommentRequestDto {
     private Long id;
+
+
+    @Schema(description = "닉네임", example = "스프링부트")
+    private String nickname;
+
     @Schema(description = "댓글", example = "안녕하세요. 반갑습니다.")
     private String comment;//댓글 내용
     private String createdDate = LocalDateTime.now()
@@ -20,15 +26,6 @@ public class CommentRequestDto {
     private String updatedDate = LocalDateTime.now()
             .format(DateTimeFormatter
             .ofPattern("yyyy.MM.dd HH:mm"));//댓글 수정 날짜
-
-    public Comment toEntity(){
-        return Comment.builder()
-                .id(id)
-                .comment(comment)
-                .createdDate(LocalDateTime.parse(createdDate))
-                .updatedDate(LocalDateTime.parse(updatedDate))
-                .build();
-    }
 
 
 }
