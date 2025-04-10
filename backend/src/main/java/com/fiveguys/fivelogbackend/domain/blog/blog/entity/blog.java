20a7c3b28314f2314b225125484cb505b5b917fb
thatisbummer <tjdals4358@naver.com>
2025-04-10 10:54:10 +0900

    feat: add: blogs fk #12

--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/blog/entity/blog.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/blog/entity/blog.java
@@ -1,16 +1,18 @@
 package com.fiveguys.fivelogbackend.domain.blog.blog.entity;
 
+
+import com.fiveguys.fivelogbackend.domain.blog.board.entity.Board;
+import com.fiveguys.fivelogbackend.domain.user.user.entity.User;
 import com.fiveguys.fivelogbackend.global.jpa.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SuperBuilder
 @MappedSuperclass
@@ -18,9 +20,16 @@ import java.time.LocalDateTime;
 @Getter
 @ToString
 @Table(name = "blogs")
-public class blog extends BaseEntity {
+public class Blog extends BaseEntity {
 
     @Column(nullable = false)
     private String title;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private List<User> userList = new ArrayList<>(); // 회원등록한 유저
+
+    @OneToMany
+    private List<Board> boardList = new ArrayList<>(); // 블로그에 등록된 게시물들
 
 }
--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/comment/entity/Comment.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/comment/entity/Comment.java
@@ -23,4 +23,5 @@ public class Comment {
     private LocalDateTime updatedDate;
 
 
+
 }
