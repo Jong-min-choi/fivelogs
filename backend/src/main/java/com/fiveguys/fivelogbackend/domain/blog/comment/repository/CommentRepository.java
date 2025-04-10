1aeb431e6d18e8265781f20e2a159dec86ba8057
dlghwns200 <dlghwns200@gmail.com>
2025-04-10 13:28:01 +0900

    feat: comment repository add #20

---
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.fiveguys.fivelogbackend.domain.blog.comment.repository;
+
+import com.fiveguys.fivelogbackend.domain.blog.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
