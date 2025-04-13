17237d7d1ac66cb46e0d2ca2964aa36f4fee4c76
0320kangk <0320kangk@naver.com>
2025-04-13 23:54:03 +0900

    blue green deploy test4

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -3,11 +3,11 @@
 # 사용자 도커 권한 주기기
 # sudo usermod -aG docker $USER 사전 설정
 # 필요한 파일에 실행 권한 부여
-chmod +x zero_downtime_deploy.py
+chmod +x scripts/zero_downtime_deploy.py
 
 # Python 스크립트 실행
 echo "Starting zero downtime deployment..."
-python3 zero_downtime_deploy.py
+python3 scripts/zero_downtime_deploy.py
 
 # 배포 후 완료 메시지
 echo "Deployment completed successfully!"
