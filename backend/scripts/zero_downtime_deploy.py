e36e995e6b5c140d13456f7732633a512eef4e51
0320kangk <0320kangk@naver.com>
2025-04-14 00:22:31 +0900

    blue green deploy test7

--- backend/scripts/zero_downtime_deploy.py
+++ backend/scripts/zero_downtime_deploy.py
@@ -44,7 +44,7 @@ class ServiceManager:
 
      # Docker 컨테이너를 실행하는 함수
     def _run_container(self, name: str, port: int) -> None:
-        os.system("aws ecr get-login-password | docker login --username AWS --password-stdin 752725210089.dkr.ecr.ap-northeast-2.amazonaws.com")
+         os.system("aws ecr get-login-password | docker login --username AWS --password-stdin 752725210089.dkr.ecr.ap-northeast-2.amazonaws.com")
          os.system(
              f"docker run -d --name={name} --restart unless-stopped -p {port}:8090 -e TZ=Asia/Seoul -v /app/volumes/gen:/gen --pull always 752725210089.dkr.ecr.ap-northeast-2.amazonaws.com/fivelogs:latest")
 
