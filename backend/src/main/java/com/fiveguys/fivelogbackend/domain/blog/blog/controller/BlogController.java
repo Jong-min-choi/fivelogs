32814836f4fad55342e60c7fa7fdf1acfcb05c0e
thatisbummer <tjdals4358@naver.com>
2025-04-17 17:02:17 +0900

    feat/add search By tagName#94

--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/blog/controller/BlogController.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/blog/controller/BlogController.java
@@ -54,4 +54,5 @@ public class BlogController {
         return ResponseEntity.ok(searchResult);
     }
 
+
 }
--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/board/controller/BoardController.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/board/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.fiveguys.fivelogbackend.domain.blog.board.controller;
 
+import com.fiveguys.fivelogbackend.domain.blog.board.entity.Board;
 import com.fiveguys.fivelogbackend.domain.blog.board.service.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,4 +29,13 @@ public class BoardController {
 
 //    @Operation(summary = "게시글 열람")
 //    }
+
+    @GetMapping("/search-tag")
+    @Operation(summary = "태그 기반 게시글 검색", description = "해시태그에 해당하는 게시글 검색")
+    public ResponseEntity<Page<Board>> searchByTag(
+            @RequestParam String tag,
+            @PageableDefault(size = 10, page = 0) Pageable pageable) {
+        Page<Board> boards = boardService.searchBoardsByHashtag(tag, pageable);
+        return ResponseEntity.ok(boards);
+    }
 }
